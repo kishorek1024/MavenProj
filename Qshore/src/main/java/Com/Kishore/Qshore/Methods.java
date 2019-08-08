15763273e896686eb60ee32ae5c07f8e1a38b222
DELL <DELL@DESKTOP-BPC4G7D>
2019-08-08 12:29:06 +0530

    Commit1

---
+++ Qshore/src/main/java/Com/Kishore/Qshore/Methods.java
@@ -0,0 +1,13 @@
+package Com.Kishore.Qshore;
+
+public class Methods {
+	
+	public void abcd(){
+		System.out.println("Method1");
+	}
+
+	public void xyz(){
+		System.out.println("Method2");
+	}
+}
+
